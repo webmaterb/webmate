18077dd2c7ee9d79d074647bf6dfa431046d6e2f
Evgeniy <malleus.petrov@gmail.com>
2013-05-29 20:27:01 +0400

    cleanup base presenter

--- lib/webmate/presenters/base_presenter.rb
+++ lib/webmate/presenters/base_presenter.rb
@@ -4,9 +4,8 @@ module Webmate
 
     attr_accessor :accessor, :resources
 
-    def initialize(resources)
-      raise ArgumentError, "Resources should not be blank" if resources.blank?
-      @resources = resources
+    def initialize(resources = [])
+      @resources = resources.to_a
       @errors = []
     end
 
@@ -55,61 +54,3 @@ module Webmate
     end
   end
 end
-=begin
-class BasePresenter
-  include Serializers::Scoped
-
-  attr_accessor :accessor, :resources
-
-  def initialize(resources)
-    raise ArgumentError, "Resources should not be blank" if resources.blank?
-    @resources = resources
-  end
-
-  def to_serializable
-    build_serialized default_resource do |object|
-      attributes object.attributes.keys
-    end
-  end
-
-  def errors
-    resource_by_name(:errors) || []
-  end
-
-  def to_json(options = {})
-    serialize_resource.to_json
-  end
-
-  private
-
-  def serialize_resource
-    if errors.present?
-      serialize_errors
-    else
-      to_serializable
-    end
-  end
-
-  def serialize_errors
-    errors = resource_by_name(:errors)
-    build_serialized do
-      namespace 'errors' do
-        errors.each do |error|
-          attribute error.key do
-            error.value
-          end
-        end
-      end
-    end
-  end
-
-  def resource_by_name(name)
-    @resources.resource(name.to_sym)
-  end
-
-  def default_resource
-    @resources.resource(:default)
-  end
-
-end
-=end
